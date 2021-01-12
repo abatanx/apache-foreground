4e06e99f25a11419ab20ebea949a2ab675d6c0af
Takao Kobayashi <abatanx@gmail.com>
2021-01-12 17:48:46 +0900

    Fix: \r\n

--- etc/apache-logfilter.pl
+++ etc/apache-logfilter.pl
@@ -7,15 +7,16 @@ sub r
 {
 	my($s) = @_;
 	if( $s =~ /x([0-9a-f]{2})/ ) { chr(hex($1)); }
-	elsif( $s eq 't' ) { chr(9);  }
-	elsif( $s eq 'r' ) { chr(10); }
-	elsif( $s eq 'n' ) { chr(13); }
+	elsif( $s eq 'n' ) { chr(13);chr(10); }
+	elsif( $s eq 't' ) { chr(9); }
 	else { '' };
 }
 
 while(<>)
 {
+	s/\\r\\n/\\n/g;
+	s/\\r/\\n/g;
 	s/^\[.+?\] \[.+?\] \[.+?\] \[.+?\] //;
-	s/\\([trn]|x[0-9a-f]{2})/r($1)/ge;
+	s/\\([tn]|x[0-9a-f]{2})/r($1)/ge;
 	print $_;
 }
